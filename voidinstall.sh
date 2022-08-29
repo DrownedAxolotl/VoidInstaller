efc15675e65563949299e6a2ea01097c3b243705
DrownedAxolotl <112387224+DrownedAxolotl@users.noreply.github.com>
2022-08-29 09:43:33 +0000

    Inital testing commit

---
+++ voidinstall.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+echo "Make sure you're online and root"
+echo "WARNING! This will erase all data on the selected drive!"
+lsblk
+echo Choose installation drive
+read diskname
+wipefs -a /dev/$diskname
+fdisk /dev/$diskname
+
+lsblk
+echo "Please enter the name of your root partition"
+read rootpartition
+echo Enter your swap name
+read swappartition
+
+mkfs.btrfs -L root /dev/$rootpartition
+mkswap /dev/$swappartition
+
+swapon /dev/$swappartition
+
+mount -o compress=zstd /dev/$rootpartition /mnt
+btrfs sub create /mnt/@
+btrfs sub create /mnt/@home
+umount /mnt
+
+mount -o compress=zstd,subvol=@ /mnt
+mkdir /mnt/home
+mount -o compress=zstd,subvol=@home /mnt/home
+
+REPO=https://repo-fi.voidlinux.org/current
+ARCH=x86_64
+
+XBPS_ARCH=$ARCH xbps-install -S -r /mnt -R "$REPO" base-system btrfs-progs
+
+mount -o bind /dev /mnt/dev
+mount -o bind /proc /mnt/proc
+mount -o bind /sys /mnt/sys
+mount -o bind /run /mnt/run
+cp /etc/resolv.conf /mnt/etc
+
+echo Please choose your hostname
+read hostname && echo $hostname >> /mnt/etc/hostname
+
+echo Entering chroot...
+read
+echo Set root password
+chroot /mnt passwd
+
+echo Choose your username
+read username
+
+echo Generating fstab
+cat << GEN | chroot /mnt
+id_root=$(blkid -s UUID -o value /dev/$rootpartition)
+id_swap=$(blkid -s UUID -o value /dev/$swappartition)
+cat << STAB >> /etc/fstab
+UUID=$id_swap none swap sw 0 0
+UUID=$id_root / btrfs subvol=/@, defaults 0 1
+UUID=$id_root /home btrfs subvol=/@home, defaults 0 1
+tmpfs /tmp tmpfs defaults,nosuid,nodev 0 0  
+STAB
+GEN
+
+ 
+
